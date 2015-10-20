9e8a3e6509e4acd4bd39d4e470238f37b8a7aa6f
Robert <robertcorreas@gmail.com>
2015-10-20 20:40:57 -0200

    Tests for serializaton with multiple relations (OneToOne-OneToMany-ManyToMany)

--- NewDatabase.Test/SerializationRequiriments/SerializationTest.cs
+++ NewDatabase.Test/SerializationRequiriments/SerializationTest.cs
@@ -218,5 +218,86 @@ namespace NewDatabase.Test.SerializationRequiriments
             Assert.True(1 == newGraphicTable.Count);
             Assert.True(0 == newTrajectoryGraphicRelationalTable.Count);
         }
+
+        [Fact]
+        public void ShouldSerializeWithOneToManyAndOneToOneAndManyToMany()
+        {
+            var wellTable = new Table<Well>(_dataTest.Wells, w => w.Id, _relation, _index);
+            var geometryTable = new Table<Geometry>(_dataTest.Geometries, g => g.Id, _relation, _index);
+            var trajectoryTable = new Table<Trajectory>(_dataTest.Trajectories, t => t.Id, _relation, _index);
+            var trajectoryPointTable = new Table<TrajectoryPoint>(_dataTest.TrajectoryPoints, tp => tp.Id, _relation,
+                _index);
+            var graphicTable = new Table<Graphic>(_dataTest.Graphics, g => g.Id, _relation, _index);
+            var trajectoryGraphicRelationalTable =
+                new Table<TrajectoryGraphicRelationalTable>(_dataTest.TrajectoryGraphicRelationalTables, tgr => tgr.Id,
+                    _relation, _index);
+
+            _relation.CreateOneToOne(wellTable, geometryTable, w => w.Geometry.Id);
+            _relation.CreateOneToOne(wellTable, trajectoryTable, w => w.Trajectory.Id);
+            _relation.CreateOneToMany(trajectoryTable, trajectoryPointTable, tp => tp.Trajectory.Id);
+            _relation.CreateManyToMany(trajectoryTable, graphicTable, trajectoryGraphicRelationalTable,
+                tgr => tgr.Trajectory.Id, tgr => tgr.Graphic.Id);
+
+
+
+            var geometry = new Geometry();
+            var trajectory = new Trajectory();
+            var well = new Well(geometry, trajectory);
+
+            var tp1 = new TrajectoryPoint(trajectory);
+            var tp2 = new TrajectoryPoint(trajectory);
+            var tp3 = new TrajectoryPoint(trajectory);
+
+            var graphic = new Graphic();
+            var trajectoryGraphic = new TrajectoryGraphicRelationalTable(trajectory, graphic);
+
+            
+
+            trajectoryTable.Insert(trajectory);
+            geometryTable.Insert(geometry);
+            wellTable.Insert(well);
+            trajectoryPointTable.Insert(tp1);
+            trajectoryPointTable.Insert(tp2);
+            trajectoryPointTable.Insert(tp3);
+            graphicTable.Insert(graphic);
+            trajectoryGraphicRelationalTable.Insert(trajectoryGraphic);
+
+
+            Assert.True(1 == wellTable.Count);
+            Assert.True(1 == geometryTable.Count);
+            Assert.True(1 == trajectoryTable.Count);
+            Assert.True(3 == trajectoryPointTable.Count);
+            Assert.True(1 == graphicTable.Count);
+            Assert.True(1 == trajectoryGraphicRelationalTable.Count);
+
+            var jsonData = JsonConvert.SerializeObject(_dataTest, Formatting.Indented);
+            var jsonIndex = JsonConvert.SerializeObject(_index, Formatting.Indented);
+
+            var newData = JsonConvert.DeserializeObject<DataTest.DataTest>(jsonData);
+            var newIndex = JsonConvert.DeserializeObject<Index>(jsonIndex);
+            var newRelation = new Relation();
+
+            var newWellTable = new Table<Well>(newData.Wells, w => w.Id, newRelation, newIndex);
+            var newGeometryTable = new Table<Geometry>(newData.Geometries, g => g.Id, newRelation, newIndex);
+            var newTrajectoryTable = new Table<Trajectory>(newData.Trajectories, t => t.Id, newRelation, newIndex);
+            var newTrajectoryPointTable = new Table<TrajectoryPoint>(newData.TrajectoryPoints, tp => tp.Id, newRelation, newIndex);
+            var newGraphicTable = new Table<Graphic>(newData.Graphics, g => g.Id, newRelation, newIndex);
+            var newTrajectoryGraphicRelationalTable = new Table<TrajectoryGraphicRelationalTable>(newData.TrajectoryGraphicRelationalTables, tgr => tgr.Id, newRelation, newIndex);
+
+            newRelation.CreateOneToOne(newWellTable, newGeometryTable, w => w.Geometry.Id);
+            newRelation.CreateOneToOne(newWellTable, newTrajectoryTable, w => w.Trajectory.Id);
+            newRelation.CreateOneToMany(newTrajectoryTable, newTrajectoryPointTable, tp => tp.Trajectory.Id);
+            newRelation.CreateManyToMany(newTrajectoryTable, newGraphicTable, newTrajectoryGraphicRelationalTable,
+                tgr => tgr.Trajectory.Id, tgr => tgr.Graphic.Id);
+
+            newWellTable.Delete(well);
+
+            Assert.True(0 == newWellTable.Count);
+            Assert.True(0 == newGeometryTable.Count);
+            Assert.True(0 == newTrajectoryTable.Count);
+            Assert.True(0 == newTrajectoryPointTable.Count);
+            Assert.True(1 == newGraphicTable.Count);
+            Assert.True(0 == newTrajectoryGraphicRelationalTable.Count);
+        }
     }
 }
