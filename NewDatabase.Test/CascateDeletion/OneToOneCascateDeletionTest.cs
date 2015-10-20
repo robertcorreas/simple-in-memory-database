808cd29261cb91268923eddb663184a3940c2051
Robert <robertcorreas@gmail.com>
2015-10-19 22:37:12 -0200

    More testes for OneToOne cascate deletion. Now for multiples one to one

--- NewDatabase.Test/CascateDeletion/OneToOneCascateDeletionTest.cs
+++ NewDatabase.Test/CascateDeletion/OneToOneCascateDeletionTest.cs
@@ -64,5 +64,45 @@ namespace NewDatabase.Test.CascateDeletion
             Assert.True(0 == wellTable.Count);
             Assert.True(1 == geometryTable.Count);
         }
+
+        [Fact]
+        public void ShouldDeleteInCascateWithMultipleOneToOne()
+        {
+            var index = new Index();
+            var relation = new Relation();
+
+            var wellTable = new Table<Well>(tuplas: _dataTest.Wells, primaryKey: w => w.Id, relation: relation, index: index);
+            var geometryTable = new Table<Geometry>(tuplas: _dataTest.Geometries, primaryKey: g => g.Id, relation: relation, index: index);
+
+            relation.CreateOneToOne(wellTable, geometryTable, w => w.Geometry.Id);
+
+            var geometry1 = new Geometry();
+            var well1 = new Well(geometry1);
+
+            var geometry2 = new Geometry();
+            var well2 = new Well(geometry2);
+
+            geometryTable.Insert(geometry1);
+            wellTable.Insert(well1);
+
+            geometryTable.Insert(geometry2);
+            wellTable.Insert(well2);
+
+            Assert.True(2 == wellTable.Count);
+            Assert.True(2 == geometryTable.Count);
+            Assert.True(4 == index.Count);
+
+            wellTable.Delete(well1);
+
+            Assert.True(1 == wellTable.Count);
+            Assert.True(1 == geometryTable.Count);
+            Assert.True(2 == index.Count);
+
+            Assert.Equal(well2.Id,wellTable.Get(well2.Id).Id);
+            Assert.Equal(geometry2.Id, wellTable.Get(well2.Id).Geometry.Id);
+            Assert.Equal(geometry2.Id, geometryTable.Get(geometry2.Id).Id);
+        }
+
+
     }
 }
